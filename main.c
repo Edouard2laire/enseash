32d9816d335df46f1621b23004e3dec7984f30a3
Edouard2laire <edouard.delaire@icloud.com>
2017-12-13 08:42:01 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+
+	char buffer[100];
+	strcpy(buffer,"Bonjour a tous");
+
+
+	int fd;
+
+
+
+
+	exit(1);
+}
